58339c41446e759c5e8630e006399acdc6dd1932
gabrieletoffanin <gabrieletoffanin@outlook.it>
2025-07-10 19:31:14 +0200

    initialize project structure with Docker support, gRPC services and SwiftUI

---
+++ src/Imagine.UI/Imagine.UI/Imagine_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imagine_UIApp.swift
+//  Imagine.UI
+//
+//  Created by Gabriele Toffanin on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Imagine_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
